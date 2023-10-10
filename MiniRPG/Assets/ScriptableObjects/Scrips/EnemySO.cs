4388f493871f753e79990a6baf9d289f1aa89eb0
jwonp9127 <wldnjs9127@naver.com>
2023-10-11 02:01:14 +0900

    [Add] Character Asset, Environment Asset

---
+++ MiniRPG/Assets/ScriptableObjects/Scrips/EnemySO.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemySO", menuName = "Characters/Enemy")]
+public class EnemySO : ScriptableObject
+{
+    [field: SerializeField] public float PlayerChasingRange { get; private set; } = 10f;
+    [field: SerializeField] public float AttackRange { get; private set; } = 1.5f;
+
+    [field: SerializeField]
+    [field: Range(0f, 3f)]
+    public float ForceTransitionTime { get; private set; }
+
+    [field: SerializeField]
+    [field: Range(-10f, 10f)]
+    public float Force { get; private set; }
+
+    [field: SerializeField] public int Damage { get; private set; }
+
+    [field: SerializeField]
+    [field: Range(0f, 1f)]
+    public float DealingStartTransitionTime { get; private set; }
+
+    [field: SerializeField]
+    [field: Range(0f, 1f)]
+    public float DealingEndTransitionTime { get; private set; }
+
+    [field: SerializeField] public PlayerGroundData GroundedData { get; private set; }
+}
